4d314c89b4c141b9b8804c8dd454c100438ede2f
Didier Osindero <didierosindero@gmail.com>
2022-12-09 08:54:22 +0000

    Add try catch statements

--- src/server.ts
+++ src/server.ts
@@ -33,38 +33,54 @@ app.get("/", (req, res) => {
 
 // GET /to-dos
 app.get("/to-dos", async (req, res) => {
-  const text = "SELECT * FROM todos";
-  const queryResponse = await client.query(text);
-  const allToDos = queryResponse.rows;
-  res.status(200).json(allToDos);
+  try {
+    const text = "SELECT * FROM todos";
+    const queryResponse = await client.query(text);
+    const allToDos = queryResponse.rows;
+    res.status(200).json(allToDos);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // POST /to-dos
 app.post<{}, {}, ItoDoText>("/to-dos", async (req, res) => {
-  const postData = req.body;
-  const text = "INSERT INTO todos (task) VALUES ($1) RETURNING *";
-  const values = [postData.task];
-  const queryResponse = await client.query(text, values);
-  const createdToDo = queryResponse.rows[0];
-  res.status(201).json(createdToDo);
+  try {
+    const postData = req.body;
+    const text = "INSERT INTO todos (task) VALUES ($1) RETURNING *";
+    const values = [postData.task];
+    const queryResponse = await client.query(text, values);
+    const createdToDo = queryResponse.rows[0];
+    res.status(201).json(createdToDo);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // GET /to-dos/:id
 app.get<{ id: string }>("/to-dos/:id", async (req, res) => {
-  const id = req.params.id;
-  const text = "SELECT * FROM todos WHERE id = $1";
-  const values = [id];
-  const queryResponse = await client.query(text, values);
-  const matchingToDo = queryResponse.rows[0];
-  res.status(200).json(matchingToDo);
+  try {
+    const id = req.params.id;
+    const text = "SELECT * FROM todos WHERE id = $1";
+    const values = [id];
+    const queryResponse = await client.query(text, values);
+    const matchingToDo = queryResponse.rows[0];
+    res.status(200).json(matchingToDo);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // DELETE /completed-to-dos
 app.delete("/completed-to-dos", async (req, res) => {
-  const text = "DELETE FROM todos WHERE complete = true RETURNING *";
-  const queryResponse = await client.query(text);
-  const returnedArr = queryResponse.rows;
-  res.status(200).json(returnedArr);
+  try {
+    const text = "DELETE FROM todos WHERE complete = true RETURNING *";
+    const queryResponse = await client.query(text);
+    const returnedArr = queryResponse.rows;
+    res.status(200).json(returnedArr);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 // PATCH /to-dos/:id
